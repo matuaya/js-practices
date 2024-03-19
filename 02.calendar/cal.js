e19a3274b496a790d4fcf7f95d43776a02d68360
matsuyama <aya.k.matsuyama@gmail.com>
2024-03-20 01:58:52 +0900

    変数にconstを使用し、変数名も適切なものに変更。表示を文字列結合からテンプレート文字列に変更。また、曜日の定数定義とadd_spacesメソッドの削除などのリファクタリンゴを行った。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,44 +1,37 @@
 #!/usr/bin/env node
 
-import { endOfMonth, isSaturday, setDate, addDays } from "date-fns";
+import { endOfMonth, isSaturday, addDays } from "date-fns";
 import minimist from "minimist";
 
-const WEEK = ["日", "月", "火", "水", "木", "金", "土"];
+const displayDates = (firstOfMonth) => {
+  const lastOfMonth = endOfMonth(firstOfMonth);
 
-let add_spaces = (target_date) => {
-  let space_count = target_date.getDay();
-  process.stdout.write("   ".repeat(space_count));
-};
-
-let display_dates = (target_date) => {
-  let first_date = setDate(target_date, 1);
-  let last_date = endOfMonth(target_date);
-
-  for (let date = first_date; date <= last_date; date = addDays(date, 1)) {
-    process.stdout.write(String(date.getDate()).padStart(2, " ") + " ");
-    if (isSaturday(date) || date.getDate() == last_date.getDate()) {
-      console.log();
+  process.stdout.write("   ".repeat(firstOfMonth.getDay()));
+  for (let date = firstOfMonth; date <= lastOfMonth; date = addDays(date, 1)) {
+    let paddedDate = String(date.getDate()).padStart(2, " ");
+    if (isSaturday(date) || date.getDate() === lastOfMonth.getDate()) {
+      console.log(paddedDate);
+    } else {
+      process.stdout.write(`${paddedDate} `);
     }
   }
 };
 
-let display_calendar = (year, month) => {
-  console.log("      " + month + "月 " + year);
-  console.log(WEEK.join(" "));
+const displayCalendar = (year, month) => {
+  console.log(`      ${month}月 ${year}\n日 月 火 水 木 金 土`);
 
-  let target_date = new Date(year, month - 1);
-  add_spaces(target_date);
-  display_dates(target_date);
+  const firstOfMonth = new Date(year, month - 1);
+  displayDates(firstOfMonth);
 };
 
-let main = () => {
-  let argv = minimist(process.argv.slice(2));
+const main = () => {
+  const argv = minimist(process.argv.slice(2));
 
-  let today = new Date();
-  let year = argv.y || today.getFullYear();
-  let month = argv.m || today.getMonth() + 1;
+  const today = new Date();
+  const year = argv.y ?? today.getFullYear();
+  const month = argv.m ?? today.getMonth() + 1;
 
-  display_calendar(year, month);
+  displayCalendar(year, month);
 };
 
 main();
