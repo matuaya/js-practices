c0b868b7c262c894f006a9dd475f7c10462c0502
matsuyama <aya.k.matsuyama@gmail.com>
2024-05-01 03:20:23 +0900

    Async/awaitベースの非同期処理にエラーハンドリングを追加。

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
@@ -29,6 +30,40 @@ const getData = (sql, param) => {
 
   const row = await getData("SELECT * FROM books WHERE title=?", ["book1"]);
   console.log(row);
+
+  runQuery("DROP TABLE books");
+})();
+
+await timers.setTimeout(1000);
+
+const getDataErrorHandling = (sql, param) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, param, (error, row) => {
+      if (error) {
+        reject(error);
+      } else resolve(row);
+    });
+  });
+};
+
+(async () => {
+  await runQuery(
+    "CREATE TABLE books(id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+  );
+  const statementObj = await runQuery("INSERT INTO books (title) VALUES (?)", [
+    "book1",
+  ]);
+  console.log(statementObj.lastID);
+
+  try {
+    const row = await getDataErrorHandling(
+      "SELECT * FROM textbooks WHERE title=?",
+      ["book1"],
+    );
+    console.log(row);
+  } catch (error) {
+    console.log(error.message);
+  }
 
   runQuery("DROP TABLE books");
 })();
