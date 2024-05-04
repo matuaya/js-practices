588ed8f3615ce359fecd46dc479f58d6e6429dd2
matsuyama <aya.k.matsuyama@gmail.com>
2024-05-04 16:53:27 +0900

    トップレベルの await を利用し、即時実行関数の使用を削除

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -6,43 +6,37 @@ import { runQuery, getData } from "./database_utils.js";
 
 const db = new sqlite3.Database(":memory:");
 
-(async () => {
-  await runQuery(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-  );
-  const statement = await runQuery(db, "INSERT INTO books (title) VALUES (?)", [
-    "book1",
-  ]);
-  console.log(statement.lastID);
+await runQuery(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+);
+const statement = await runQuery(db, "INSERT INTO books (title) VALUES (?)", [
+  "book1",
+]);
+console.log(statement.lastID);
 
-  const row = await getData(db, "SELECT * FROM books WHERE title = ?", [
-    "book1",
-  ]);
-  console.log(row);
+const row = await getData(db, "SELECT * FROM books WHERE title = ?", ["book1"]);
+console.log(row);
 
-  runQuery(db, "DROP TABLE books");
-})();
+runQuery(db, "DROP TABLE books");
 
 await timers.setTimeout(1000);
 
-(async () => {
-  await runQuery(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-  );
+await runQuery(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+);
 
-  try {
-    await runQuery(db, "INSERT INTO books (name) VALUES (?)", ["book1"]);
-  } catch (error) {
-    console.error(error.message);
-  }
+try {
+  await runQuery(db, "INSERT INTO books (name) VALUES (?)", ["book1"]);
+} catch (error) {
+  console.error(error.message);
+}
 
-  try {
-    await getData(db, "SELECT * FROM textbooks WHERE title = ?", ["book1"]);
-  } catch (error) {
-    console.error(error.message);
-  }
+try {
+  await getData(db, "SELECT * FROM textbooks WHERE title = ?", ["book1"]);
+} catch (error) {
+  console.error(error.message);
+}
 
-  runQuery(db, "DROP TABLE books");
-})();
+runQuery(db, "DROP TABLE books");
