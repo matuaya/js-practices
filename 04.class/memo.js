54300ca35e3571f82e353161655063e1f75ee011
matsuyama <aya.k.matsuyama@gmail.com>
2024-06-27 02:28:30 +0900

    実行スクリプトからメモ操作のロジックをMemoServiceに移動

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -3,74 +3,18 @@
 import MemoJsonStorage from "./memo_json_storage.js";
 import MemoRepository from "./memo_repository.js";
 import MemoService from "./memo_service.js";
-import { readUserInput, selectPrompt } from "./memo_prompt.js";
 
 const option = process.argv[2];
 const storage = new MemoJsonStorage("./memos.json");
 const repository = new MemoRepository(storage);
 const service = new MemoService(repository);
 
-const showMemoList = async () => {
-  if (!(await repository.dataExists())) {
-    console.log("No memos found");
-
-    return;
-  }
-  await service.showList();
-};
-
-const showMemoContent = async () => {
-  if (!(await repository.dataExists())) {
-    console.log("No memos found");
-
-    return;
-  }
-  const memos = await repository.getMemos();
-  const prompt = await selectPrompt("Choose a memo you want to see", memos);
-  try {
-    const selectedId = await prompt.run();
-    await service.showFullContent(selectedId);
-  } catch (error) {
-    if (error === "") {
-      console.log("No memo selected");
-    } else {
-      throw error;
-    }
-  }
-};
-
-const deleteMemo = async () => {
-  if (!(await repository.dataExists())) {
-    console.log("No memos found");
-
-    return;
-  }
-  const memos = await repository.getMemos();
-  const prompt = await selectPrompt("choose a memo you want to delete", memos);
-  try {
-    const selectedId = await prompt.run();
-    await service.delete(selectedId);
-    console.log("Memo deleted successfully");
-  } catch (error) {
-    if (error === "") {
-      console.log("No memo selected");
-    } else {
-      throw error;
-    }
-  }
-};
-
-const addMemo = async () => {
-  const inputData = await readUserInput();
-  await service.add(inputData);
-};
-
 if (option === "-l") {
-  showMemoList();
+  service.showList();
 } else if (option === "-r") {
-  showMemoContent();
+  service.showFullContent();
 } else if (option === "-d") {
-  deleteMemo();
+  service.delete();
 } else {
-  addMemo();
+  service.add();
 }
--- 04.class/memo_service.js
+++ 04.class/memo_service.js
@@ -1,25 +1,69 @@
+import { readUserInput, selectPrompt } from "./memo_prompt.js";
+
 export default class MemoService {
   constructor(repository) {
     this.repository = repository;
   }
 
-  add(inputData) {
-    return this.repository.add(inputData);
+  async add() {
+    const inputData = await readUserInput();
+    this.repository.add(inputData);
   }
 
-  delete(id) {
-    return this.repository.delete(id);
+  async delete() {
+    if (!(await this.repository.dataExists())) {
+      console.log("No memos found");
+
+      return;
+    }
+    const memos = await this.repository.getMemos();
+    const prompt = await selectPrompt(
+      "choose a memo you want to delete",
+      memos,
+    );
+    try {
+      const selectedId = await prompt.run();
+      await this.repository.delete(selectedId);
+      console.log("Memo deleted successfully");
+    } catch (error) {
+      if (error === "") {
+        console.log("No memo selected");
+      } else {
+        throw error;
+      }
+    }
   }
 
   async showList() {
+    if (!(await this.repository.dataExists())) {
+      console.log("No memos found");
+
+      return;
+    }
     const memos = await this.repository.getMemos();
     memos.forEach((memo) => {
       console.log(memo.firstLine());
     });
   }
 
-  async showFullContent(id) {
-    const memo = await this.repository.getMemo(id);
-    console.log(memo.content);
+  async showFullContent() {
+    if (!(await this.repository.dataExists())) {
+      console.log("No memos found");
+
+      return;
+    }
+    const memos = await this.repository.getMemos();
+    const prompt = await selectPrompt("Choose a memo you want to see", memos);
+    try {
+      const selectedId = await prompt.run();
+      const memo = await this.repository.getMemo(selectedId);
+      console.log(memo.content);
+    } catch (error) {
+      if (error === "") {
+        console.log("No memo selected");
+      } else {
+        throw error;
+      }
+    }
   }
 }
